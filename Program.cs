70f29ee75343d904702a6e4e956f05a5836b31ce
Ovais Mehboob Ahmed Khan <ovaismehboob@hotmail.com>
2020-10-13 14:21:04 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WitQuickStarts
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                ShowUsage();
+                return 0;
+            }
+
+            string connectionUrl, token, project = "";
+
+            try
+            {
+                CheckArguments(args, out connectionUrl, out token, out project);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+
+                ShowUsage();
+
+                return -1;
+            }
+
+            try
+            {
+                Console.WriteLine("Executing quick start samples...");
+                Console.WriteLine("");
+
+                //todo: may need to adjust this to scale better for more samples.
+
+                #region Bug
+                //Instantiating CreateBug object
+                Samples.CreateBug objBug = new Samples.CreateBug(connectionUrl, token, project);
+                //execute the client lib code. If you want to run the direct http calls then adjust (see below)
+                objBug.CreateBugUsingClientLib();
+                objBug = null;
+                #endregion
+
+                #region Epic
+                //Instantiating CreateEpic object
+                Samples.CreateEpic epic = new Samples.CreateEpic(connectionUrl, token, project);
+                epic.CreateEpicUsingClientLib();
+                epic= null;
+                #endregion
+
+                #region Feature
+                //Instantiating CreateFeature object 
+                Samples.CreateFeature feature = new Samples.CreateFeature(connectionUrl, token, project);
+                feature.CreateFeatureUsingClientLib();
+                feature = null;
+                #endregion
+
+                #region Execute Query 
+                //Instantiating ExecuteQuery object  
+                Samples.ExecuteQuery objQuery = new Samples.ExecuteQuery(connectionUrl, token, project);
+                objQuery.RunGetBugsQueryUsingClientLib();
+                objQuery = null;
+                #endregion
+                  
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to run the sample: " + ex.Message);
+                return 1;
+            }
+
+            return 0;
+        }
+
+        private static void ShowUsage()
+        {
+            Console.WriteLine("Runs the WIT Quick Start samples on a Team Services account or Team Foundation Server instance.");
+            Console.WriteLine("");
+            Console.WriteLine("These samples are to provide you the building blocks of using the REST API's in Work Item Tracking.");
+            Console.WriteLine("Examples are written using the .NET client library and using direct HTTP calls. We recommend, that");
+            Console.WriteLine("whenever possible, you use the .NET client library.");
+            Console.WriteLine("");
+            Console.WriteLine("!!WARNING!! Some samples are destructive. Always run on a test account or collection.");
+            Console.WriteLine("");
+            Console.WriteLine("Arguments:");
+            Console.WriteLine("");
+            Console.WriteLine("  /url:fabrikam.visualstudio.com /token:personalaccesstoken /project:projectname");
+            Console.WriteLine("");
+
+            Console.ReadKey();
+        }
+
+        private static void CheckArguments(string[] args, out string connectionUrl, out string token, out string project)
+        {
+            connectionUrl = null;
+            token = null;
+            project = null;
+            
+            Dictionary<string, string> argsMap = new Dictionary<string, string>();
+            foreach (var arg in args)
+            {
+                if (arg[0] == '/' && arg.IndexOf(':') > 1)
+                {
+                    string key = arg.Substring(1, arg.IndexOf(':') - 1);
+                    string value = arg.Substring(arg.IndexOf(':') + 1);
+
+                    switch (key)
+                    {
+                        case "url":
+                            connectionUrl = value;
+                            break;
+
+                        case "token":
+                            token = value;                          
+                            break;
+
+                        case "project":
+                            project = value;
+                            break;
+                        default:
+                            throw new ArgumentException("Unknown argument", key);
+                    }
+                }
+            }
+
+            if (connectionUrl == null || token == null)
+            {
+                throw new ArgumentException("Missing required arguments");
+            }
+        }
+
+
+    }
+}
---
+++ Samples/CreateFeature.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.TeamFoundation.WorkItemTracking.WebApi;
+using Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models;
+using Microsoft.VisualStudio.Services.Common;
+using Microsoft.VisualStudio.Services.WebApi;
+using Microsoft.VisualStudio.Services.WebApi.Patch;
+using Microsoft.VisualStudio.Services.WebApi.Patch.Json;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+
+namespace WitQuickStarts.Samples
+{
+    public class CreateFeature
+    {
+        readonly string _uri;
+        readonly string _personalAccessToken;
+        readonly string _project;
+
+        public CreateFeature()
+        {
+            _uri = "https://accountname.visualstudio.com";
+            _personalAccessToken = "personal access token";
+            _project = "project name";
+        }
+
+        /// <summary>
+        /// Constructor. Manaully set values to match your account.
+        /// </summary>
+        public CreateFeature(string url, string pat, string project)
+        {
+            _uri = url;
+            _personalAccessToken = pat;
+            _project = project;
+        }
+
+        /// <summary>
+        /// Create a feature using the .NET client library
+        /// </summary>
+        /// <returns>Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models.WorkItem</returns>    
+        public WorkItem CreateFeatureUsingClientLib()
+        {
+            Uri uri = new Uri(_uri);
+            string personalAccessToken = _personalAccessToken;
+            string project = _project;
+
+            VssBasicCredential credentials = new VssBasicCredential("", _personalAccessToken);
+            JsonPatchDocument patchDocument = new JsonPatchDocument();
+
+            //add fields and thier values to your patch document
+            patchDocument.Add(
+                new JsonPatchOperation()
+                {
+                    Operation = Operation.Add,
+                    Path = "/fields/System.Title",
+                    Value = "Test Feature 1"
+                }
+            );
+
+
+
+            VssConnection connection = new VssConnection(uri, credentials);
+            WorkItemTrackingHttpClient workItemTrackingHttpClient = connection.GetClient<WorkItemTrackingHttpClient>();
+
+            try
+            {
+                WorkItem result = workItemTrackingHttpClient.CreateWorkItemAsync(patchDocument, project, "Feature").Result;
+
+                Console.WriteLine("Feature Successfully Created: Feature #{0}", result.Id);
+
+                return result;
+            }
+            catch (AggregateException ex)
+            {
+                Console.WriteLine("Error creating Feature: {0}", ex.InnerException.Message);
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Create a feature using direct HTTP
+        /// </summary>     
+        public WorkItem CreateFeatureUsingHTTP()
+        {
+            string uri = _uri;
+            string personalAccessToken = _personalAccessToken;
+            string project = _project;
+            string credentials = Convert.ToBase64String(System.Text.ASCIIEncoding.ASCII.GetBytes(string.Format("{0}:{1}", "", personalAccessToken)));
+
+            Object[] patchDocument = new Object[1];
+
+            patchDocument[0] = new { op = "add", path = "/fields/System.Title", value = "Test Feature" };
+
+            using (var client = new HttpClient())
+            {
+                //set our headers
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", credentials);
+
+                //serialize the fields array into a json string
+                var patchValue = new StringContent(JsonConvert.SerializeObject(patchDocument), Encoding.UTF8, "application/json-patch+json");
+
+                var method = new HttpMethod("PATCH");
+                var request = new HttpRequestMessage(method, uri + "/" + project + "/_apis/wit/workitems/$Feature?api-version=2.2") { Content = patchValue };
+                var response = client.SendAsync(request).Result;
+
+                //if the response is successfull, set the result to the workitem object
+                if (response.IsSuccessStatusCode)
+                {
+                    var workItem = response.Content.ReadAsAsync<WorkItem>().Result;
+
+                    Console.WriteLine("Epic Successfully Created: Feature #{0}", workItem.Id);
+                    return workItem;
+                }
+                else
+                {
+                    Console.WriteLine("Error creating Feature: {0}", response.Content);
+                    return null;
+                }
+            }
+        }
+    }
+}
